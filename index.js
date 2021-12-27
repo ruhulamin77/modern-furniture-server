96d76d16f1bb798f9e6b5d9e9defebd5bcf28290
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2021-12-27 15:44:24 +0600

    my orders done

--- index.js
+++ index.js
@@ -71,6 +71,14 @@ async function run() {
       const result = await cursor.toArray();
       res.json(result);
     });
+
+    // delete single order
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.json(result);
+    });
   } finally {
     // await client.close();
   }
